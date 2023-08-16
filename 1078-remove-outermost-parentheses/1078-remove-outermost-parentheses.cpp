42b3336bff933a9ac435e43746ad519d64ee1ce8
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2023-08-16 16:15:23 +0530

    Time: 0 ms (100%), Space: 6.7 MB (76.37%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    string removeOuterParentheses(string S) {
-        string res;
-        int opened = 0;
-        for (char c : S) {
-            if (c == '(' && opened++ > 0) res += c;
-            if (c == ')' && opened-- > 1) res += c;
+    string removeOuterParentheses(string s) {
+        //keeping track of opened
+        //opened --> represent no. of opened without closed 
+        //"(()())(())"
+        // "((" --> opened=2; 
+        // "(()"  --> opened=1; 
+        string ans;
+        int opened=0;
+        for(auto i:s){
+            if(i=='(' && opened++>0)    ans+=i;
+            if(i==')' && opened-->1)    ans+=i;    
         }
-        return res;
+        return ans;
     }
 };
