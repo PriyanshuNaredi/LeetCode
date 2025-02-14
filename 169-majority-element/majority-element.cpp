6fd47812aa208bc9a0cd4047df36b339bb3fcba3
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2025-02-14 09:54:40 -0500

    Time: 0 ms (100.00%) | Memory: 28.1 MB (58.32%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        /* Better Sol */
+        /* Better Sol
         map <int,int> mpp;
         for (int i = 0; i < nums.size(); i++){
             mpp[nums[i]]++;
@@ -11,6 +11,37 @@ public:
             if ( it.second > ( nums.size() / 2))
                 return it.first;
         }
+
+        return -1;
+        */
+
+        /* Optimal Sol */
+        int cnt=0, el;
+
+        for (int i = 0; i < nums.size(); i++){
+            if (cnt == 0){
+                cnt = 1;
+                el = nums[i];
+            }
+            else if ( nums[i] == el){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+
+        int cntt = 0;
+        for (int i=0; i<nums.size(); i++){
+            if (nums[i] == el ){
+                cntt++;
+            }
+        }
+
+        if ( cntt > (nums.size() /2 )){
+            return el;
+        }
+
 
         return -1;
     }
