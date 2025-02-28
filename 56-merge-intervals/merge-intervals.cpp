030026252f80dfd5b079fee510ba549b9b80ce9b
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2025-02-28 10:28:30 -0500

    Time: 0 ms (100.00%) | Memory: 23.7 MB (92.65%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -8,12 +8,14 @@ public:
             int start = a[i][0];
             int end = a[i][1];
             if (!ans.empty() && end <= ans.back()[1]) {
+                // already lying inside the interval
                 continue;
             }
             for (int j = i + 1; j < n; j++) {
                 if (a[j][0] <= end) {
+                    // cheak if lying inside then update the upper bound
                     end = max(end, a[j][1]);
-                } else
+                } else // create new set
                     break;
             }
             ans.push_back({start, end});
@@ -26,6 +28,7 @@ public:
         vector<vector<int>> ans;
         for (int i = 0; i < n; i++) {
             if (ans.empty() || a[i][0] > ans.back()[1]){
+                // doesnt belong to existing interval
                 ans.push_back(a[i]);
             }else{
                 ans.back()[1] = max(ans.back()[1], a[i][1]);
