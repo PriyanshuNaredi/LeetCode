ed8141b5596788ab88cff7d614979c6970f9abd1
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2025-05-30 08:34:31 -0400

    Time: 1 ms (53.85%) | Memory: 10.2 MB (77.22%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+string result2(string s){
+        string result = "";
+        int end = s.size() - 1;
+
+        while (end >= 0) {
+            while (end >= 0 && s[end] == ' ') {
+                end--;
+            }
+
+            if (end < 0) {
+                break;
+            }
+
+            if (!result.empty()) {
+                result += " ";
+            }
+
+            int start = end;
+            while (start >= 0 && s[start] != ' ') {
+                start--;
+            }
+
+            result += s.substr(start + 1, end - start);
+            end = start;
+        }
+
+        return result;
+}
+string result(string s)
+{
+        string temp="",ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' '){
+                temp+=s[i];
+            }
+            else if(s[i]==' ' && temp!=""){
+                ans=temp+" "+ans;
+                temp="";
+            }
+            else{
+                continue;
+            }
+        }
+        if(temp!=""){
+            ans=temp+" "+ans;
+        }
+
+        return ans.substr(0,ans.size()-1); // at last there is an emplty space present so we remeove it by  using str function
+ 
+}
+    string reverseWords(string s) {
+        return result2(s);
+    }
+};
