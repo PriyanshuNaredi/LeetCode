44662c7a8cfe1f5b8d310be0a4e6836929c9b55a
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2025-05-30 23:48:03 -0400

    Time: 0 ms (100.00%) | Memory: 17.2 MB (91.90%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        for(int i = num.length(); i >= 0; i--)
-        {
-            if(((int)num[i]) % 2 != 0)
-            {
-                // here return i+1 as the last index is excluded while using the substr function
-                return num.substr(0, i+1);
+        // directly check id the last digit is odd then the whole no is odd.
+        int i = num.length() - 1;
+        if (((int)num[i]) % 2 != 0) {
+            cout << "Direct ";
+            return num;
+        }
+
+        for (int i = num.length(); i >= 0; i--) {
+            if (((int)num[i]) % 2 != 0) {
+                // here return i+1 as the last index is excluded while using the
+                // substr function
+                return num.substr(0, i + 1);
             }
         }
         return "";
     }
 };
-
