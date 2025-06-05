683e80fbb61f4f79845377f6359850772ebcc5c8
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2025-06-05 07:10:45 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.25%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,28 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        i = 0
+        summ = 0
+
+        while i < len(s):
+            if i < len(s) - 1 and d[s[i]] < d[s[i+1]]:
+                # IV = V - I 
+                #  4 = 5 - 1
+                # Therefore, check the next char and if i < i+1 
+                summ += d[s[i+1]] - d[s[i]]
+                i += 2
+            else:
+                summ += d[s[i]]
+                i += 1
+        
+        return summ
+        
