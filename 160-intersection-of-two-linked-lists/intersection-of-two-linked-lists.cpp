2defeb74aa2ed2cbf68cb1624fd707baf35b79c0
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2025-08-08 18:40:10 +0530

    Time: 28 ms (96.73%) | Memory: 18.7 MB (23.50%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -11,6 +11,25 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
         // B.F
+        /*
+        ListNode* head1 = headA;
+        ListNode* head2 = headB;
+        while(head2 != NULL) {
+        ListNode* temp = head1;
+        while(temp != NULL) {
+            //if both nodes are same
+            if(temp == head2) return head2;
+            temp = temp->next;
+        }
+        head2 = head2->next;
+    }
+    //intersection is not present between the lists return null
+    return NULL;
+
+    */
+
+        // Better
+        /*
         ListNode* head1 = headA;
         ListNode* head2 = headB;
 
@@ -24,6 +43,32 @@ public:
             head2 = head2->next;
         }
         return NULL;
+
+        */
+
+        // optimal
+
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+
+        while (temp1 != temp2) {
+            temp1 = (temp1 == nullptr) ? headB : temp1->next;
+
+            if(temp2 == NULL){
+                temp2 = headA;
+            }else{
+                temp2 = temp2->next;
+            }
+        } 
+
+        //1. move both temp1 and temp2 until they reach the end
+        //2. when they reach the end make them point to the alternate
+        // list heads
+        //3. continue until they collide
+        // P.S if there is no intersection then while loop will stop
+        // when both temp's points at NULL/nullptr
+
+        return temp1;    
         
     }
 };
