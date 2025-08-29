ba78c039d8f472d10f3c12ea6f27b5328318c2e8
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2025-08-30 02:54:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (43.37%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,25 +1,28 @@
 class Solution {
-public:
-    double myPow(double x, int n) {
-
+    double iterSol(double x, int n) {
         double ans = 1.0;
         long long nn = n;
 
-        if(nn<0)
+        if (nn < 0)
             nn = -1 * nn;
-        
-        while(nn){
-            if(nn%2){
+
+        while (nn) {
+            if (nn % 2) {
                 ans = ans * x;
                 nn = nn - 1;
-            }else{
+            } else {
                 x = x * x;
                 nn = nn / 2;
             }
         }
-        if(n<0)
-            ans = (double) (1.0) / (double) (ans);
+        if (n < 0)
+            ans = (double)(1.0) / (double)(ans);
 
         return ans;
+    }
+
+public:
+    double myPow(double x, int n) {
+        return iterSol(x, n);
     }
 };
