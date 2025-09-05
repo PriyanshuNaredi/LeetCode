3e0d5f60574445aa76938544ccb2a44a1542b5dd
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2025-09-05 19:42:52 +0530

    Time: 0 ms (100%), Space: 35.8 MB (31.74%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int total = std::accumulate(nums.begin(), nums.end(), 0);
+        int left = 0;
+        for (int i = 0; i < n; i++) {
+            int right = total - left - nums[i];
+            if (left == right)
+                return i;
+            left += nums[i];
+        }
+        return -1;
+    }
+};
