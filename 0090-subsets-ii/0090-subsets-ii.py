11936898bb4aaf9f7f4f6f82f135aba8d9bd588b
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2025-09-17 19:56:42 +0530

    Time: 0 ms (100%), Space: 18.1 MB (38.4%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,21 +1,36 @@
-from typing import List
-
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        nums.sort()  # sort so duplicates are adjacent
+        ans = set() # stores only one copy 
+        nums.sort() # not required
         
-        def solve(idx, path):
-            ans.append(path[:])  # add a copy of current subset
-            for i in range(idx, len(nums)):
-                # skip duplicates
-                if i > idx and nums[i] == nums[i-1]:
-                    continue
-                path.append(nums[i])
-                solve(i+1, path)
-                path.pop()  # backtrack
+        def solve(ip, op):
+            if not ip:
+                ans.add(tuple(op))  # store as tuple as list are not hashable in python
+                return
+            
+            # exclude
+            solve(ip[1:], op)
+            
+            # include
+            solve(ip[1:], op + [ip[0]])
         
-        solve(0, [])
-        return ans
+        solve(nums, [])
+        
+        # convert back to list of lists
+        return [list(t) for t in ans]
 
+"""
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        
+        def solve(ip, op):
+            if not ip:
+                ans.append(op)
+                return
+            solve(ip[1:], op)          # exclude current element
+            solve(ip[1:], op + [ip[0]]) # include current element
+        
+        solve(nums, [])
+        return ans
+"""
         
