2e913c5f50ed48c4f9d94e89d7f0c5b373d37d9c
Priyanshu Naredi <82588790+PriyanshuNaredi@users.noreply.github.com>
2026-01-23 17:48:49 +0530

    Time: 27 ms (18.1%), Space: 30.2 MB (5.15%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,81 @@
+class Solution {
+
+    vector<int> nSL(vector<int>& heights) {
+        stack<pair<int, int>> st; // {arr[i],i}
+        vector<int> v;
+        for (int i = 0; i < heights.size(); i++) {
+            if (st.size() == 0)
+                v.push_back(-1);
+            else if (st.size() > 0 and st.top().first < heights[i])
+                v.push_back(st.top().second);
+            else if (st.size() > 0 and st.top().first >= heights[i]) {
+                while (st.size() > 0 and st.top().first >= heights[i])
+                    st.pop();
+                if (st.size() == 0)
+                    v.push_back(-1);
+                else
+                    v.push_back(st.top().second);
+            }
+            st.push({heights[i], i});
+        }
+        return v;
+    }
+    vector<int> nSR(vector<int>& heights) {
+        stack<pair<int, int>> st; // {arr[i],i}
+        vector<int> v;
+        for (int i = heights.size() - 1; i >= 0; i--) {
+            if (st.size() == 0)
+                v.push_back(heights.size());
+            else if (st.size() > 0 and st.top().first < heights[i])
+                v.push_back(st.top().second);
+            else if (st.size() > 0 and st.top().first >= heights[i]) {
+                while (st.size() > 0 and st.top().first >= heights[i])
+                    st.pop();
+                if (st.size() == 0)
+                    v.push_back(heights.size());
+                else
+                    v.push_back(st.top().second);
+            }
+            st.push({heights[i], i});
+        }
+        reverse(v.begin(), v.end());
+        return v;
+    }
+    int MAH(vector<int>& heights) {
+        // Nearest Smaller to left
+        vector<int> left = nSL(heights);
+        // Nearest Greater to right
+        vector<int> right = nSR(heights);
+        vector<int> width(heights.size());
+        vector<int> area(heights.size());
+        int maxArea = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            width[i] = right[i] - left[i] - 1;
+        }
+        for (int i = 0; i < heights.size(); i++) {
+            area[i] = width[i] * heights[i];
+            maxArea = max(area[i], maxArea);
+        }
+        return maxArea;
+    }
+
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        vector<int> v;
+        for (int j = 0; j < matrix[0].size(); j++) {
+            v.push_back(matrix[0][j] - '0');
+        }
+        int mx = MAH(v);
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == '0')
+                    v[j] = 0;
+                else {
+                    v[j] = v[j] + (matrix[i][j] - '0');
+                }
+            }
+            mx = max(mx, MAH(v));
+        }
+        return mx;
+    }
+};
